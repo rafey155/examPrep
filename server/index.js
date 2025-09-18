09887e211e0dd31c902a30bc648bb72d9e3f268e
Mohd Rafey <rafeymohd155@gmail.com>
2025-09-18 23:26:05 +0530

    CORS url fix

--- server/index.js
+++ server/index.js
@@ -1,37 +1,43 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://exam-prep-frontend.vercel.app/", // TODO: Replace with your actual frontend URL
+    credentials: true,
+  })
+);
 app.use(express.json());
-const URL ='mongodb://localhost:27017/examprep'
+const URL = "mongodb://localhost:27017/examprep";
 
-mongoose.connect(URL)
-.then(() => {
-    console.log('Connected to MongoDB')
-})
-.catch((er)=> {
-    console.log(`Error is ${er}`)
-});
+mongoose
+  .connect(URL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((er) => {
+    console.log(`Error is ${er}`);
+  });
 
 // api start
 
-app.use('/api/examinee',require('./routes/examineeRoute'))
-app.use('/api/admin',require('./routes/adminRoute'))
+app.use("/api/examinee", require("./routes/examineeRoute"));
+app.use("/api/admin", require("./routes/adminRoute"));
 
 // session api
-app.use('/api/session/',require('./routes/sessionRoute'))
+app.use("/api/session/", require("./routes/sessionRoute"));
 // subject api
-app.use('/api/subject',require('./routes/subjectRoute'))
+app.use("/api/subject", require("./routes/subjectRoute"));
 // question api
-app.use('/api/question/',require('./routes/questionRoute'))
+app.use("/api/question/", require("./routes/questionRoute"));
 // exam api
-app.use('/api/exams/',require('./routes/examinationRoute'))
+app.use("/api/exams/", require("./routes/examinationRoute"));
 // message api
-app.use('/api/message',require('./routes/messageRoute'))
+app.use("/api/message", require("./routes/messageRoute"));
 
 // api end
-app.listen(5000,()=>{
-    console.log('Server is running on http://localhost:5000');
-})
+app.listen(5000, () => {
+  console.log("Server is running on http://localhost:5000");
+});
