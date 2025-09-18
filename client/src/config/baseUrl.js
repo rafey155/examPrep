a4c29680fe680c3dec62ce869b75902387f116d5
Mohd Rafey <rafeymohd155@gmail.com>
2025-09-19 01:18:15 +0530

    url change

--- client/src/config/baseUrl.js
+++ client/src/config/baseUrl.js
@@ -2,6 +2,6 @@
 // Centralized base URL config for client
 
 const BASE_URL =
-  import.meta.env.VITE_BASE_URL || "https://exam-prep-backend.vercel.app/";
+  import.meta.env.VITE_BASE_URL || "https://exam-prep-backend.vercel.app";
 
 export default BASE_URL;
